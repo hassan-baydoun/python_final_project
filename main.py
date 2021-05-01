679e6f0c35a7b7bcb863cb5960b9e46b77462ebe
hassan-baydoun <57936729+hassan-baydoun@users.noreply.github.com>
2021-05-01 17:23:41 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+import streamlit as st
+import pandas as pd
+import time
+
+a = st.sidebar.text('Welcome!')
+
+st.title('Welcome to my Final Python Project!')
+st.subheader('Presented to: Prof. Georges Salloum by Hassan BAYDOUN (192604)')
