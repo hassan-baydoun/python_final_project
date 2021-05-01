9562fe7802d1fb374d91e68808cb7bf40a5a2535
hassan-baydoun <57936729+hassan-baydoun@users.noreply.github.com>
2021-05-01 20:35:43 +0300

    Implementing Custom Yolov5

--- main.py
+++ main.py
@@ -1,8 +1,92 @@
+from contextlib import contextmanager
+from io import StringIO
+from streamlit.report_thread import REPORT_CONTEXT_ATTR_NAME
+from threading import current_thread
 import streamlit as st
 import pandas as pd
 import time
+from detect import detect
+import os
+import sys
+import argparse
+
+@contextmanager
+def st_redirect(src, dst):
+    placeholder = st.empty()
+    output_func = getattr(placeholder, dst)
+
+    with StringIO() as buffer:
+        old_write = src.write
+
+        def new_write(b):
+            if getattr(current_thread(), REPORT_CONTEXT_ATTR_NAME, None):
+                buffer.write(b)
+                output_func(buffer.getvalue())
+            else:
+                old_write(b)
+
+        try:
+            src.write = new_write
+            yield
+        finally:
+            src.write = old_write
+
+
+@contextmanager
+def st_stdout(dst):
+    with st_redirect(sys.stdout, dst):
+        yield
+
+
+@contextmanager
+def st_stderr(dst):
+    with st_redirect(sys.stderr, dst):
+        yield
+
+def all_subdirs_of(b='.'):
+    result = []
+    for d in os.listdir(b):
+        bd = os.path.join(b, d)
+        if os.path.isdir(bd): result.append(bd)
+    return result
+
+def get_latest_folder():
+    return max(all_subdirs_of('runs\detect'), key=os.path.getmtime)
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--weights', nargs='+', type=str, default='weights\yolov5m.pt', help='model.pt path(s)')
+parser.add_argument('--source', type=str, default='data/images', help='source')  # file/folder, 0 for webcam
+parser.add_argument('--img-size', type=int, default=640, help='inference size (pixels)')
+parser.add_argument('--conf-thres', type=float, default=0.25, help='object confidence threshold')
+parser.add_argument('--iou-thres', type=float, default=0.45, help='IOU threshold for NMS')
+parser.add_argument('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
+parser.add_argument('--view-img', action='store_true', help='display results')
+parser.add_argument('--save-txt', action='store_true', help='save results to *.txt')
+parser.add_argument('--save-conf', action='store_true', help='save confidences in --save-txt labels')
+parser.add_argument('--save-crop', action='store_true', help='save cropped prediction boxes')
+parser.add_argument('--nosave', action='store_true', help='do not save images/videos')
+parser.add_argument('--classes', nargs='+', type=int, help='filter by class: --class 0, or --class 0 2 3')
+parser.add_argument('--agnostic-nms', action='store_true', help='class-agnostic NMS')
+parser.add_argument('--augment', action='store_true', help='augmented inference')
+parser.add_argument('--update', action='store_true', help='update all models')
+parser.add_argument('--project', default='runs/detect', help='save results to project/name')
+parser.add_argument('--name', default='exp', help='save results to project/name')
+parser.add_argument('--exist-ok', action='store_true', help='existing project/name ok, do not increment')
+parser.add_argument('--line-thickness', default=3, type=int, help='bounding box thickness (pixels)')
+parser.add_argument('--hide-labels', default=False, action='store_true', help='hide labels')
+parser.add_argument('--hide-conf', default=False, action='store_true', help='hide confidences')
+opt = parser.parse_args()
 
 a = st.sidebar.text('Welcome!')
 
 st.title('Welcome to my Final Python Project!')
 st.subheader('Presented to: Prof. Georges Salloum by Hassan BAYDOUN (192604)')
+
+if st.button('Hit me'):
+    with st_stdout("info"):
+        detect(opt)
+    for img in os.listdir(get_latest_folder()):
+            if img.endswith(".jpg" or ".png"):
+                st.image(f'{get_latest_folder()}\{img}')
+
+
